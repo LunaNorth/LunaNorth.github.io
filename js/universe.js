9094748433da7ba3b51930b43d0351671aff2e27
LunaNorth <169933505@qq.com>
2024-08-06 08:58:07 +0800

    Site updated: 2024-08-06 08:58:07

--- js/universe.js
+++ js/universe.js
@@ -4,14 +4,14 @@ dark()
 //动态标题
 var OriginTitile = document.title;
 var titleTime;
-document.addEventListener('visibilitychange', function () {
+document.addEventListener("visibilitychange", function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '👀跑哪里去了~';
+    document.title = "w(ﾟДﾟ)w 不要走！再看看嘛！";
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🐖抓到你啦～';
+    document.title = "♪(^∇^*)欢迎肥来！" + OriginTitile;
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
